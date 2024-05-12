75d0e5f0d031d524fe545f9b66ebb6c4b79ddd26
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-05-12 17:29:05 +0100

    Fixed name changes

--- backend/src/main/java/com/apollocare/backend/controller/ApolloController.java
+++ backend/src/main/java/com/apollocare/backend/controller/ApolloController.java
@@ -9,36 +9,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.apollocare.backend.models.Appointment;
-import com.apollocare.backend.service.AppoimentService;
+import com.apollocare.backend.models.Consultation;
+import com.apollocare.backend.service.ConsultationService;
 
 @RestController
 public class ApolloController {
 
-    private final AppoimentService apService;
+    private final ConsultationService cService;
 
-    public ApolloController(AppoimentService apService) {
-        this.apService = apService;
+    public ApolloController(ConsultationService cService) {
+        this.cService = cService;
     }
 
-    @GetMapping("/appointments")
-    public List<Appointment> getAllAppointments() {
-        return apService.findAllAppointments();
+    @GetMapping("/consultations")
+    public List<Consultation> getAllConsultations() {
+        return cService.findAllConsultations();
     }
 
     @PostMapping("/add")
-    public Appointment addAppointment(@RequestBody Appointment appointment) {
-        return apService.schedule(appointment);
+    public Consultation addConsultation(@RequestBody Consultation Consultation) {
+        return cService.schedule(Consultation);
     }
 
-    @GetMapping("/appointments/{id}")
-    public Appointment getAppointmentById(@PathVariable Long id) {
-        return apService.getAppointmentById(id).orElse(null);
+    @GetMapping("/consultations/{id}")
+    public Consultation getConsultationById(@PathVariable Long id) {
+        return cService.getConsultationById(id).orElse(null);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteAppointment(@PathVariable Long id) {
-        apService.deleteAppointment(id);
+    public void deleteConsultation(@PathVariable Long id) {
+        cService.deleteConsultation(id);
     }
 
 }
