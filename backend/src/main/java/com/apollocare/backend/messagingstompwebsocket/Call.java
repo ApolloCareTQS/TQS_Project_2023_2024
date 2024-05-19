622fe2fcc9afa98cf684d3641ec03003f6e8f5e8
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-05-19 09:50:07 +0100

    KAN-44 fixed code duplication

--- backend/src/main/java/com/apollocare/backend/messagingstompwebsocket/Call.java
+++ backend/src/main/java/com/apollocare/backend/messagingstompwebsocket/Call.java
@@ -1,40 +1,16 @@
 package com.apollocare.backend.messagingstompwebsocket;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Call {
-    private String callType; 
+    private String callType;
     private Long callNumber;
     private String callPost;
-
-    public Call(String callType, Long callNumber, String callPost) {
-        this.callType = callType;
-        this.callNumber = callNumber;
-        this.callPost = callPost;
-    }
-
-    public Call() {
-    }
-
-    public String getCallType() {
-        return callType;
-    }
-
-    public void setCallType(String callType) {
-        this.callType = callType;
-    }
-
-    public Long getCallNumber() {
-        return callNumber;
-    }
-
-    public void setCallNumber(Long callNumber) {
-        this.callNumber = callNumber;
-    }
-
-    public String getCallPost() {
-        return callPost;
-    }
-
-    public void setCallPost(String callPost) {
-        this.callPost = callPost;
-    }
 }
--- backend/src/main/java/com/apollocare/backend/messagingstompwebsocket/CallMessage.java
+++ backend/src/main/java/com/apollocare/backend/messagingstompwebsocket/CallMessage.java
@@ -1,49 +1,17 @@
 package com.apollocare.backend.messagingstompwebsocket;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CallMessage {
     private String callType; 
     private Long callNumber;
     private String callPost;
     private String timestamp;
-
-    public CallMessage() {}
-
-    public CallMessage(String callType, Long callNumber, String callPost, String timestamp) {
-        this.callType = callType;
-        this.callNumber = callNumber;
-        this.callPost = callPost;
-        this.timestamp = timestamp;
-    }
-
-    public String getCallType() {
-        return callType;
-    }
-
-    public void setCallType(String callType) {
-        this.callType = callType;
-    }
-
-    public Long getCallNumber() {
-        return callNumber;
-    }
-
-    public void setCallNumber(Long callNumber) {
-        this.callNumber = callNumber;
-    }
-
-    public String getCallPost() {
-        return callPost;
-    }
-
-    public void setCallPost(String callPost) {
-        this.callPost = callPost;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp= timestamp;
-    }
 }
