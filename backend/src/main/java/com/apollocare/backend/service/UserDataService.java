83241ed36a02b7cc6779a0b40845d0ba8340f2a6
BrunoPascoa76 <brunopascoa03@ua.pt>
2024-05-21 11:01:41 +0100

    KAN-45 added missing Service tag

--- backend/src/main/java/com/apollocare/backend/service/UserDataService.java
+++ backend/src/main/java/com/apollocare/backend/service/UserDataService.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 import com.apollocare.backend.models.Consultation;
 import com.apollocare.backend.models.ConsultationRepo;
 
+@Service
 public class UserDataService {
     private ConsultationRepo consultationRepo;
     private static final Logger logger=LogManager.getLogger();
